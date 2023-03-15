6791082c20b90258405b34e88fa40e8bd05f2cd1
pikolos <84343397+pikolos@users.noreply.github.com>
2023-03-15 21:14:29 +0100

    Update create_resourcepool.sh

--- compute/create_resourcepool.sh
+++ compute/create_resourcepool.sh
@@ -41,6 +41,6 @@ sed -i -e "s/###VCENTER###/${VCENTER}/" \
 ${SCRIPT}
 
 echo "Creating ResourcePool '${HEADER}-${1}' with ${4} Nested ESXi."
-docker run --rm --dns=${DNS} --entrypoint="/usr/bin/pwsh" -v /tmp/scripts:/tmp/scripts vmware/powerclicore:12.4 ${SCRIPT} 2>&1 > /dev/null
+docker run --rm --dns=${DNS} --entrypoint="/usr/bin/pwsh" -v /tmp/scripts:/tmp/scripts vmware/powerclicore:12.4 ${SCRIPT}
 
 #rm -fr ${SCRIPT}
